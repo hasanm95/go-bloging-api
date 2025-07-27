bd412269eea3d63c39f1494c34e726b02d781586
hasan-deriv <abuhasan@deriv.com>
2025-07-27 17:43:48 +0800

    create basic api handler

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	mux := mux.NewRouter()
+
+	mux.HandleFunc("/", apiHandler)
+
 	log.Println("Server running at 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Calling api handler")
 }
